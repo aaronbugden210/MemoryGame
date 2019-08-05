354f62675db3e89515cc3398ba7336cf822520ff
aaronbugden210 <aaronbugden@gmail.com>
2019-08-05 16:26:51 -0400

    Made image objects within the array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,24 @@
-var imgArr = new Array("cowboy-bebop-0.jpg", "cowboy-bebop-1.jpg",
-                "cowboy-bebop-2.jpg", "cowboy-bebop-3.jpg", "cowboy-bebop-4.jpg", "cowboy-bebop-5.jpg");
+var imgArr = new Array({image: "cowboy-bebop-0.jpg", wasClicked: false}, {image: "cowboy-bebop-1.jpg", wasClicked: false}, {image: "cowboy-bebop-2.jpg", wasClicked: false},
+                    {image: "cowboy-bebop-3.jpg", wasClicked: false}, {image: "cowboy-bebop-4.jpg", wasClicked: false}, {image: "cowboy-bebop-5.jpg", wasClicked: false},
+                    {image: "cowboy-bebop-6.jpg", wasClicked: false});
 
 var basePath = "assets/imgs/";
 
-var row1 = $("<div class='row'></div>");
-var row2 = $("<div class='row'></div>");
+var row = $("<div class='row'></div>");
+
+imgRandom();
 
 function imgRandom()
 {
-    $("#images").append(row1);
+    $("#images").append(row);
     sortArray();
     for (var i = 0; i < imgArr.length; i++) 
     {
-        // var rand = imgArr[Math.floor(Math.random() * imgArr.length)];
-        var image = new Image();
-        //image.src = basePath + rand;
-        image.src = basePath + imgArr[i];
-        $(image).css({
-            "height": "200px", 
-            "width": "200px", 
-            "margin": "10px 10px"});
+        var image = new Image(200, 200);
+        image.src = basePath + imgArr[i].image;
+        $(image).css({"margin": "10px 10px"});
         $(image).addClass("col-lg-3");
-        $(row1).append(image);
-        console.log("test test");
-        if(i >= 3 && i <= 5)
-        {
-            $("#images").append(row2);
-            $(row2).append(image);
-        }
+        $(row).append(image);
     }
 }
 
@@ -39,7 +30,23 @@ function sortArray()
     });
 }
 
-imgRandom();
+/////////////////////////
+//create a function that resets the wasClicked value's of each img to 'false'
+//it will be used on the resetGame button click
+/////////////////////////
+
+$("#resetGame").on("click", function()
+{
+    $(row).empty();
+    console.log("row emptied");
+    imgRandom();
+});
+
+$("img").on("click", function()
+{
+    this.wasClicked = true;
+    console.log(this.wasClicked);
+});
 
 //////////////////////////
 //Next objective: create button object, give it on("click") event so that it randomizes the images whenever any button is clicked,
